2189594c52aadbf7cc274845000f3fd60bc2a5b5
Eden Chambers <EdenChambers5@gmail.com>
2019-02-04 13:36:09 -0700

    I added my TextFields and Labels.

--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -14,12 +14,36 @@ public class PokedexPanel extends JPanel
 	private PokedexController pokeController;
 	private SpringLayout appLayout;
 	
+	private JTextField PichuText;
+	private JTextField EeveeText;
+	private JTextField SquirtleText;
+	private JTextField TogepiText;
+	private JTextField TorchicText; 
+	
+	private JLabel PichuLabel;
+	private JLabel EeveeLabel; 
+	private JLabel SquirtleLabel;
+	private JLabel TogepiLabel; 
+	private JLabel TorchicLabel; 
+	
 	public PokedexPanel(PokedexController pokeController)
 	{
 		super();
 		
 		this.pokeController = pokeController;
 		appLayout = new SpringLayout();
+		
+		PichuText = new JTextField("Pichu is...");
+		EeveeText = new JTextField("Eevee is...");
+		SquirtleText = new JTextField("Squirtle is...");
+		TogepiText = new JTextField("Togepi is...");
+		TorchicText = new JTextField("Torchic is...");
+		
+		PichuLabel = new JLabel("Pichu");
+		EeveeLabel = new JLabel ("Eevee");
+		SquirtleLabel = new JLabel("Squirtle");
+		TogepiLabel = new JLabel("Togepi");
+		TorchicLabel = new JLabel("Torchic");
 		
 		setupPanel();
 		setupLayout();
